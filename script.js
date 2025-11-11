c014f74562c2b45fdaf7e05b9bb3a6d484527f10
pinkLemon3 <meriblu1211@gmail.com>
2025-11-11 22:29:09 +0000

    Created 3 new buttons in order to get a node list and iterate over it using a forEach loop and logging the id of each button

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ button.addEventListener("click", () => {
     alert("Hello, World! <3");
 })
 
-// 
+// logging the event
 btn.addEventListener("click", function (e) {
     console.log(e);
 });
 
-
+// logging the event target
 btn.addEventListener("click", function (e) {
   console.log(e.target);
 });
-
+// changing the target's background colour
 btn.addEventListener("click", function (e) {
   e.target.style.background = "blue";
+});
+
+const buttons = document.querySelectorAll("button");
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+    });
 });
