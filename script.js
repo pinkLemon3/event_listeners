a140a9e52f1baaa875e8a3d5c7ef2340d579d590
pinkLemon3 <meriblu1211@gmail.com>
2025-11-11 21:33:35 +0000

    Added event listeners to the button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let button = document.querySelector("#btn"); 
+let button = document.querySelector("#btn");
 button.addEventListener("click", () => {
     alert("Hello, World! <3");
 })
+
+// 
+btn.addEventListener("click", function (e) {
+    console.log(e);
+});
+
+
+btn.addEventListener("click", function (e) {
+  console.log(e.target);
+});
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
